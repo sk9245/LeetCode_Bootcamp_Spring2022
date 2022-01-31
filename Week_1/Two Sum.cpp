8ad4e3b96597a84775bdc892d544c85806ebd801
sk9245 <90946291+sk9245@users.noreply.github.com>
2022-01-31 13:42:15 -0500

    Two Sum

---
+++ Week_1/Two Sum.cpp
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/two-sum/
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp;
+        vector<int> ans;
+        
+        for(int i = 0; i <nums.size(); i++){
+            int temp = target - nums[i];
+            if(mp.find(temp) != mp.end()){
+                ans.push_back(mp[temp]);
+                ans.push_back(i);
+                break;
+            }
+            else{
+                mp.insert(make_pair(nums[i],i));
+            }
+            
+        }
+        
+        return ans;
+    }
+};
