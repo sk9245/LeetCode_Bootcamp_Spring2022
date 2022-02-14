189e70d9badc13f226a9e48a8a55c9e87253d701
sk9245 <90946291+sk9245@users.noreply.github.com>
2022-02-14 00:43:54 -0500

    Intersection of Two Linked Lists

---
+++ Week_3/Intersection of Two Linked Lists.cpp
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/intersection-of-two-linked-lists/
+
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode* a = headA;
+        ListNode* b = headB;
+        while(a != b){
+            if(a == NULL){
+                a = headB;
+            }
+            else{
+                a = a -> next;
+            }
+            if(b == NULL){
+                b = headA;
+            }
+            else{
+                b = b -> next;
+            }
+        }
+        return a;
+    }
+};
