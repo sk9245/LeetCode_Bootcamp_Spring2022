9ec768c93aab1725698e24b3c3f6ab107bced81c
sk9245 <90946291+sk9245@users.noreply.github.com>
2022-02-28 11:37:08 -0500

    Same Tree

---
+++ Week_5/Same Tree.cpp
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/same-tree/
+
+class Solution {
+public:
+    bool helper(TreeNode* l, TreeNode* r)
+    {
+        if(!l || !r) return l == r;
+        if(l->val != r->val) return false;
+        
+        bool left = helper(l->left,r->right);
+        bool right = helper(l->right,r->left);
+        
+        return (left && right);
+    }
+    
+    bool isSymmetric(TreeNode* root) {
+        return helper(root->left,root->right);
+    }
+};
