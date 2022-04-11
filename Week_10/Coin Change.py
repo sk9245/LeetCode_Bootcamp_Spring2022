b8ace117b5962ae06a2373769ef74dced6ff4fc6
sk9245 <90946291+sk9245@users.noreply.github.com>
2022-04-10 23:48:44 -0400

    Coin Change

---
+++ Week_10/Coin Change.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/coin-change/
+
+class Solution(object):
+    def coinChange(self, coins, amount):
+        dp = [float('inf')] * (amount + 1)
+        dp[0] = 0
+        
+        for coin in coins:
+            for i in range(coin, amount + 1):
+                dp[i] = min(dp[i], dp[i - coin] + 1)
+                
+        return dp[amount] if dp[amount] != float('inf') else -1
+        
